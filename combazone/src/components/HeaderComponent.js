a1f0ef055f7990413754967407030e1ba188f5bf
jung_developer <kjhkjh0592@gmail.com>
2021-12-23 22:31:48 +0900

    [feat] 스크린, 컴포넌트 파일 예시 생성

---
+++ combazone/src/components/HeaderComponent.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+const HeaderComponent = (props) => {
+    const [isLogin, setIsLogin] = React.useState(false);
+
+    return (
+        isLogin
+    );
+}
+
+export default HeaderComponent;
---
+++ combazone/src/screens/Login.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+const Login = (props) => {
+    const [isLogin, setIsLogin] = React.useState(false);
+
+    return (
+        isLogin
+    );
+}
+
+export default Login;
